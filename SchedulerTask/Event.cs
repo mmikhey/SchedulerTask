964c56858ebe2ab0c6060f46d07ec2bade3f438b
alexdyogtev <alex.dyogtev@yandex.ru>
2016-04-18 20:45:15 +0300

    add Event and corrent Front

---
+++ SchedulerTask/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SchedulerTask
+{
+    /// <summary>
+    /// Событие
+    /// </summary>
+    class Event
+    {
+        private DateTime time;
+        private Operation operation;
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="operation">Операция, для которой создаётся событие</param>
+        /// <param name="time">Время возникновения события</param>
+        public Event(Operation operation, DateTime time)
+        {
+            this.operation = operation;
+            this.time = time;
+        }
+
+        public Operation Operation
+        {
+            get { return operation; }
+            set { }
+        }
+
+        public DateTime Time
+        {
+            get { return time; }
+            set { }
+        }
+    }
+}
